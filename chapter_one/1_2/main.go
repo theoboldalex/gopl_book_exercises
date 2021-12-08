cc7a816edf6443313325d8d64dd41d59f97bf266
theoboldalex <theoboldalex@gmail.com>
2021-12-08 20:00:00 +0000

    go fmt

--- chapter_one/1_2/main.go
+++ chapter_one/1_2/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-    for i, arg := range os.Args[1:] {
-        fmt.Printf("%d: %s\n", i, arg)
-    }
+	for i, arg := range os.Args[1:] {
+		fmt.Printf("%d: %s\n", i, arg)
+	}
 }
--- chapter_one/1_3/main.go
+++ chapter_one/1_3/main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "time"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-    // print each cmdline arg using a loop
-    startLoop := time.Now()
+	// print each cmdline arg using a loop
+	startLoop := time.Now()
 
-    for _, arg := range os.Args[1:] {
-        fmt.Println(arg)
-    }
+	for _, arg := range os.Args[1:] {
+		fmt.Println(arg)
+	}
 
-    diffLoop := time.Since(startLoop).Seconds()
+	diffLoop := time.Since(startLoop).Seconds()
 
-    fmt.Printf("Loop took %f seconds to execute.\n", diffLoop)
+	fmt.Printf("Loop took %f seconds to execute.\n", diffLoop)
 
+	// print each cmdline arg using strings.Join
+	startJoin := time.Now()
 
-    // print each cmdline arg using strings.Join
-    startJoin := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
 
-    fmt.Println(strings.Join(os.Args[1:], " "))
+	diffJoin := time.Since(startJoin).Seconds()
 
-    diffJoin := time.Since(startJoin).Seconds()
-
-    fmt.Printf("Join took %f seconds to execute.\n", diffJoin)
+	fmt.Printf("Join took %f seconds to execute.\n", diffJoin)
 }
