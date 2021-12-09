8bbe7949a4b128c7d91ef412a1dedf5eba7eae9f
theoboldalex <theoboldalex@gmail.com>
2021-12-09 23:06:29 +0000

    exercises 1.7 1.8 & 1.9 complete

--- chapter_one/1_6/fetch.go
+++ chapter_one/1_6/fetch.go
@@ -1,18 +1,26 @@
-// 1.7:
-// 1.8:
-// 1.9:
+// 1.7: refactor to io.Copy(dst, src) to copy resp.Body to stdout
+// 1.8: check for protocol prefix in args and prepend if necessary using strings.HasPrefix
+// 1.9: modify the program to also print the response status code
 
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const protocol = "https://"
 
 func main() {
 	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, protocol) {
+			url = protocol + url
+		}
+
 		resp, err := http.Get(url)
 
 		if err != nil {
@@ -20,14 +28,8 @@ func main() {
 			os.Exit(1)
 		}
 
-		b, err := ioutil.ReadAll(resp.Body)
+		io.Copy(os.Stdout, resp.Body)
+		io.Copy(os.Stdout, strings.NewReader(resp.Status))
 		resp.Body.Close()
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s %v\n", url, err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("%s", b)
 	}
 }
