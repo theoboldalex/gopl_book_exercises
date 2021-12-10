23aab70d09082dbc42b377c8cece9387be739d52
theoboldalex <theoboldalex@gmail.com>
2021-12-10 22:27:00 +0000

    copy output to file

--- chapter_one/1_7/fetchall.go
+++ chapter_one/1_7/fetchall.go
@@ -9,24 +9,38 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 	ch := make(chan string)
+
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch) // start a goroutine
 	}
-	for range os.Args[1:] {
-		fmt.Println(<-ch) // receive from channel ch
+
+	f, err := os.Create("fetchall_output.txt")
+
+	if err != nil {
+		panic(err)
 	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+	defer f.Close()
+
+	for range os.Args[1:] {
+		io.Copy(f, strings.NewReader(<-ch+"\n")) // receive from channel ch
+	}
+
+	io.Copy(f,
+		strings.NewReader(fmt.Sprintf("%.2fs elapsed\n", time.Since(start).Seconds())))
 }
 
 func fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
+
 	if err != nil {
 		ch <- fmt.Sprint(err) // send to channel ch
 		return
@@ -34,10 +48,12 @@ func fetch(url string, ch chan<- string) {
 
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close() // don't leak resources
+
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s: %v", url, err)
 		return
 	}
+
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
 }
