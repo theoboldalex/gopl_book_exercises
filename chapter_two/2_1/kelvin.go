fd98bf69df7b985d846c4eaac8bcb1178fb2483e
theoboldalex <theoboldalex@gmail.com>
2021-12-18 11:42:58 +0000

    kelvin conversion helpers

--- chapter_two/2_1/kelvin.go
+++ chapter_two/2_1/kelvin.go
@@ -2,8 +2,24 @@
 // processing temps in the kelvin scale
 package kelvin
 
-import "fmt"
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
 
-func main() {
-	fmt.Println("kelvin-go")
-}
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converts a Celsius temperature to Kelvin
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// KToC converts a Kelvin temperature to Celsius
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// FToK converts a Fahrenheit temperature to Kelvin
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f-32)*5/9 + 273.15) }
+
+// KToF converts a Kelvin temperature to Fahrenheit
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit((k-273.15)*9/5 + 32) }
