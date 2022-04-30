1644a4667609ed8e564b612bdaf19c2e3d11c535
theoboldalex <theoboldalex@gmail.com>
2022-04-30 19:50:19 +0100

    Setup exercise file with comment explainer

---
+++ chapter_two/2_2/main.go
@@ -0,0 +1,13 @@
+// Ex 2.2
+// Write a general purpose unit conversion program that reads values from
+// command line arguments (or stdin if there are none) and converts each
+// number intothe following units...
+// celsius, fahrenheit, feet, meters, pounds and kilograms
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
